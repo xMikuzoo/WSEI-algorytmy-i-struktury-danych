3fdeadf6f493d392107605e1f190a546f5f8c4cb
Wojciech Król <105241268+xMikuzoo@users.noreply.github.com>
2026-03-03 20:35:56 +0100

    implement main program logic to count right triangles within a given range

--- algorytmy-i-struktury-danych/Program.cs
+++ algorytmy-i-struktury-danych/Program.cs
@@ -1,3 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿namespace algorytmy_i_struktury_danych;
 
-Console.WriteLine("Hello, World!");
+public static class Program
+{
+    public static void Main()
+    {
+        var input = Console.ReadLine();
+        var parts = input?.Split();
+        var n = int.Parse(parts?[0] ?? "");    
+        var m = int.Parse(parts?[1] ?? "");
+        var count = 0;
+        for(var a = n; a <= m; a++)
+        {
+            for(var b = a; b <= m; b++)
+            {
+                var cSquared = (a * a) + (b * b) ;
+                var c = (int)Math.Sqrt(cSquared);
+                if (c <= m && c * c == cSquared)
+                {
+                    count++;
+                }
+            }
+        }
+        Console.WriteLine(count);
+    }
+}
