a5d605328d9cb2eac1be112c1ae10010d3e48559
Wojciech Król <105241268+xMikuzoo@users.noreply.github.com>
2026-03-26 18:42:19 +0100

    implement single linked list with Node class and print method

---
+++ algorytmy-i-struktury-danych/Node.cs
@@ -0,0 +1,11 @@
+﻿namespace algorytmy_i_struktury_danych;
+
+public class Node<T>
+{    
+    public T Data {get; set;}
+    public Node<T> Next {get; set;}
+    public Node(T data, Node<T> next = null) {
+        Data = data; Next = next;
+    }
+    public override string ToString() => (this==null)? "null" : $"{Data} -> ";
+}
--- algorytmy-i-struktury-danych/Program.cs
+++ algorytmy-i-struktury-danych/Program.cs
@@ -1,3 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿namespace algorytmy_i_struktury_danych;
+public static class Program
+{
+    public static void Main()
+    {
+        Node<int> head1 = 
+            new Node<int>(2,
+                new Node<int>(5,
+                    new Node<int>(1)));
+        PrintSingleLinkedList<int>(head1);
+    }
 
-Console.WriteLine("Hello, World!");
+    public static void PrintSingleLinkedList<T>(Node<T> head)
+    {
+        var current = head;
+        Console.Write("head -> ");
+        while (current != null)
+        {
+            Console.Write(current);
+            current = current.Next;
+        }
+        Console.WriteLine("null");
+    }
+}
