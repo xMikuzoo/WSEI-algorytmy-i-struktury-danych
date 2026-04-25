9852f03382dc8dee22cb222434f7aab55314d4db
Wojciech Król <105241268+xMikuzoo@users.noreply.github.com>
2026-04-25 12:37:30 +0200

    implement Josephus problem solution in Main method

--- algorytmy-i-struktury-danych/Program.cs
+++ algorytmy-i-struktury-danych/Program.cs
@@ -1,3 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System;
 
-Console.WriteLine("Hello, World!");
+class Program {
+    static void Main() {
+        var parts = Console.ReadLine().Split();
+        long n = long.Parse(parts[0]);
+        long m = long.Parse(parts[1]);
+        long k = long.Parse(parts[2]);
+        
+        long res = 0;
+        long fullModLimit = Math.Min(k - 1, n);
+        for (long j = 2; j <= fullModLimit; j++) {
+            res = (res + k) % j;
+        }
+        long subStart = Math.Max(k, 2);
+        for (long j = subStart; j <= n; j++) {
+            res += k;
+            if (res >= j) res -= j;
+        }
+        
+        long ans = (res + m - 1) % n + 1;
+        Console.WriteLine(ans);
+    }
+}
